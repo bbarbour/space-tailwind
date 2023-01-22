a7b6df43119a8e1611b53d7caae72b9dab757239
Bernard Barbour <bbarbour@cimpress.com>
2023-01-22 16:47:18 -0500

    75% there

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,13 +1,65 @@
 import React from 'react'
-import Navigation
- from '../components/Navigation'
+import Navigation from '../components/Navigation'
+import { useState} from 'react'
+import Anasari  from '../assets/crew/image-anousheh-ansari.png'
+import Hurly from '../assets/crew/image-douglas-hurley.png'
+import Mark from '../assets/crew/image-mark-shuttleworth.png'
+import Victor from '../assets/crew/image-victor-glover.png'
+import data from '../data/data.json'
+
+
 function Crew() {
+    const [crew, setCrew] = useState(0)
+    console.log(data.crew[crew])
+
+    const imgUrls = [ Hurly, Mark, Victor, Anasari]
+    function handleCrewClick(e) {
+        setCrew(e)
+    }
+
+
     return (
-        <div className="bg-crew-mobile min-h-screen   md:bg-crew-tablet xl:bg-crew-desktop  bg-no-repeat bg-cover flex">
+        <div className="bg-tour-mobile min-h-screen   md:bg-tour-tablet xl:bg-tour-desktop  bg-no-repeat bg-cover flex">
             <div className="mx-auto my-auto min-w-full block p-4 m-8 mt-0 ">
                 <Navigation />
+                <div className="text-white font-barlow-condensed text-3xl tracking-widest uppercase min-w-full mt-10 ml-10 xl:mt-32 xl:ml-32">
+                    <span className="text-white/25 pr-3">02</span>
+                    Meet your Crew
+                </div>
+                <div className="block lg:flex">
 
-                <div className="text-white font-bellefair text-3xl uppercase min-w-full">Hello Crew</div>
+                <div className="w-2/3 text-white  mx-auto p-3 mt-10 lg:p-6 lg:mt-20 lg:ml-10" >
+                        <div >
+                          
+                                <div className="font-bellefair uppercase text-3xl text-spaceGray  pt-2">
+                                    {data.crew[crew].role}
+                                </div>
+                        
+                        <div className="">
+                            <h1 className="mx-auto mt-3  font-bellefair uppercase text-white text-6xl  tracking-widest"> 
+                            {data.crew[crew].name}</h1>
+                        </div>
+                  
+                        <div className="mx-auto  pt-10 block">
+                           <div className="font-barlow text-spaceGray text-2xl pt-2">
+                                   {data.crew[crew].bio}
+                                </div>
+                           </div>
+            
+                        </div>
+                        <div className="flex mx-auto justif-around m-10 w-[400px] ">
+                            <button href='#' className='uppercase tracking-[2.7px] rounded-full w-4 h-4 mx-1 bg-white'  onClick={() => { handleCrewClick(0); }}></button>
+                            <button href='#' className='uppercase tracking-[2.7px] rounded-full w-4 h-4 mx-1 bg-white'  onClick={() => { handleCrewClick(1); }}></button>
+                            <button href='#' className='uppercase tracking-[2.7px] rounded-full w-4 h-4 mx-1 bg-white' onClick={() => { handleCrewClick(2); }}></button>
+                            <button href='#' className='uppercase tracking-[2.7px] rounded-full w-4 h-4 mx-1 bg-white' onClick={() => { handleCrewClick(3); }}></button>
+                        
+                        </div>
+                    </div>
+                    <div className="mx-auto my-auto w-1/3 mt-16 lg:p-6 lg:m-5 lg:ml-20 lg:mt-20" >
+                        <img src={imgUrls[crew]} alt="moon"></img>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,15 +1,75 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
-import { useState} from 'react'
+import { useState } from 'react'
+import LaunchLandscape from '../assets/technology/image-launch-vehicle-landscape.jpg'
+import LaunchPortrait from '../assets/technology/image-launch-vehicle-portrait.jpg'
+import CapsuleLandscape from '../assets/technology/image-space-capsule-landscape.jpg'
+import CapsulePotrait from '../assets/technology/image-space-capsule-portrait.jpg'
+import SpaceportLandscape from '../assets/technology/image-spaceport-landscape.jpg'
+import SpaceportPotrait from '../assets/technology/image-spaceport-portrait.jpg'
 import data from '../data/data.json'
 
+
 function Technology() {
+    const [tech, setTech] = useState(0)
+    console.log(data.technology[tech])
+
+    const imgUrlsPotrait = [LaunchPortrait, SpaceportPotrait, CapsulePotrait]
+    const imgUrlsLandscape = [LaunchLandscape, SpaceportLandscape, CapsuleLandscape]
+
+    function handleCrewClick(e) {
+        setTech(e)
+    }
+
+
     return (
-        <div className="bg-tech-mobile min-h-screen   md:bg-tech-tablet xl:bg-tech-desktop  bg-no-repeat bg-cover flex">
+        <div className="bg-tour-mobile min-h-screen   md:bg-tour-tablet xl:bg-tour-desktop  bg-no-repeat bg-cover flex">
             <div className="mx-auto my-auto min-w-full block p-4 m-8 mt-0 ">
                 <Navigation />
+                <div className="text-white font-barlow-condensed text-3xl tracking-widest uppercase min-w-full mt-10 ml-10 xl:mt-32 xl:ml-32">
+                    <span className="text-white/25 pr-3">03</span>
+                    Space launch 101
+                </div>
 
-                <div className="text-white font-bellefair text-3xl uppercase min-w-full">Hello Technology</div>
+                <div className="block lg:flex">
+                    <div className="text-white mx-auto px-0 py-3 mt-10 lg:p-6 lg:mt-20 lg:ml-10" >
+                         <div className="min-w-full  mx-auto xl:hidden" >
+                                <img src={imgUrlsLandscape[tech]} width="100%" alt="moon"></img>
+                        </div>
+                        <div className="w-1/2 mx-auto block">
+                            <div className="flex lg:block mx-auto justify-around min-w-[180px] lg:min-w-[30px] lg:w-[30px] ">
+                                <button href='#' className='uppercase rounded-full w-16 h-16 mx-1 lg:m-2 text-xl text-darkGray bg-white' onClick={() => { handleCrewClick(0); }}>1</button>
+                                <button href='#' className='uppercase rounded-full w-16 h-16 mx-1 lg:m-2 text-xl text-darkGray bg-white' onClick={() => { handleCrewClick(1); }}>2</button>
+                                <button href='#' className='uppercase rounded-full w-16 h-16 mx-1 lg:m-2 text-xl text-darkGray bg-white' onClick={() => { handleCrewClick(2); }}>3</button>
+                             
+                            </div>
+                        </div>
+                    </div>
+                    <div className="block xl:flex">
+                        <div className="block p-4 xl:flex xl:p-2 ">
+                            <div className='block mx-auto xl:mx-10'>
+                                <div className="font-barlow uppercase text-xl text-spaceGray  pt-2">
+                                    The terminology
+                                </div>
+
+                                <div className="mx-auto ">
+                                    <h1 className="mx-auto mt-3  font-bellefair uppercase text-white text-4xl lg:text-6xl  tracking-widest">
+                                        {data.technology[tech].name}</h1>
+                                </div>
+
+                                <div className="mx-auto  pt-10 block">
+                                    <div className="font-barlow text-spaceGray text-2xl pt-2">
+                                        {data.technology[tech].description}
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="min-w-[400px] min-h-[400px] ml-3 pt-4 mx-auto hidden lg:flex" >
+                                <img src={imgUrlsPotrait[tech]} alt="moon"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
